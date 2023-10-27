e75377e159cb1bbeaf33b5d891fb03cbee4a5a7b
Harry Ingham <98830464+No-1-Goblin@users.noreply.github.com>
2023-10-27 15:05:39 +0100

    Added name text below penguin

--- Networking Project/PlayerBase.cpp
+++ Networking Project/PlayerBase.cpp
@@ -7,6 +7,7 @@ PlayerBase::PlayerBase() {
 	penguinColour.setOrigin(penguinColour.getSize() * 0.5f);
 	font.loadFromFile("font/BUMBASTIKA.ttf");
 	usernameText.setFont(font);
+	usernameText.setCharacterSize(10);
 }
 
 PlayerBase::~PlayerBase() {
@@ -20,6 +21,10 @@ void PlayerBase::init(sf::Texture* penguinBaseTex, sf::Texture* penguinColourTex
 	penguinColour.setFillColor(data.colour);
 	penguinBase.setPosition(data.x, data.y);
 	penguinColour.setPosition(data.x, data.y);
+	usernameText.setString(data.name);
+	usernameText.setOrigin(usernameText.getGlobalBounds().getSize() / 2.f + usernameText.getLocalBounds().getPosition());
+	usernameText.setPosition(data.x, data.y + 100);
+	usernameText.setFillColor(data.colour);
 }
 
 void PlayerBase::update(float dt) {
@@ -28,4 +33,5 @@ void PlayerBase::update(float dt) {
 void PlayerBase::render(sf::RenderWindow* window) {
 	window->draw(penguinBase);
 	window->draw(penguinColour);
+	window->draw(usernameText);
 }
