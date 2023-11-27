1d426e361d99cc38ca796a76ea76a47460e9a7b5
Harry Ingham <98830464+No-1-Goblin@users.noreply.github.com>
2023-11-27 15:23:12 +0000

    Began commenting code

--- Networking Project/ChatWindow.cpp
+++ Networking Project/ChatWindow.cpp
@@ -1,8 +1,10 @@
 #include "ChatWindow.h"
 
 ChatWindow::ChatWindow() {
+	// Load font for chat messages
 	font.loadFromFile("font/arial.ttf");
 	isChatting = false;
+	// Fill the messages vector with empty messages for positioning reasons
 	for (int i = 0; i < 16; i++) {
 		sf::Text* message = new sf::Text();
 		message->setFont(font);
@@ -11,19 +13,24 @@ ChatWindow::ChatWindow() {
 		message->setFillColor(sf::Color::Black);
 		messages.push_back(message);
 	}
+	// Add default server join messages=
 	ChatMessageData msg;
 	msg.sender = "SERVER";
 	msg.colour = sf::Color::White;
 	msg.message = "Welcome to the server!";
 	addMessage(msg);
+	// Set colour of and size of chat window background
 	background.setFillColor(sf::Color(0, 0, 0, 100));
 	background.setSize(sf::Vector2f(640, 280));
+	// Set variables for text displaying how to chat/currently typed chat message
 	myMessage = "Press 'T' to chat";
 	myMessageText.setFont(font);
 	myMessageText.setCharacterSize(14);
 	myMessageText.setString(myMessage);
 	myMessageText.setFillColor(sf::Color::White);
 	myMessageText.setPosition(4, 262);
+
+	// Define what characters turn into which when shift is held
 	shiftSymbols[0] = 41;
 	shiftSymbols[1] = 33;
 	shiftSymbols[2] = 34;
@@ -47,6 +54,8 @@ ChatWindow::ChatWindow() {
 	shiftSymbols[19] = 43;
 	shiftSymbols[20] = 95;
 
+	// Used to simplify code for non-shift symbols (i.e square brackets and the like)
+	// SFML has these values in sequence for their enums, but ASCII does not, so this acts as translation layer
 	symbols[0] = 91;
 	symbols[1] = 93;
 	symbols[2] = 59;
@@ -64,13 +73,16 @@ ChatWindow::~ChatWindow() {
 
 }
 
+// Handles all key presses
 ChatMessageData* ChatWindow::update(float dt) {
 	ChatMessageData* returnVal = nullptr;
 	if (isChatting) {
+		// Add a space if space is pressed
 		bool isShiftDown = (keyboard->isKeyDown(sf::Keyboard::LShift) || keyboard->isKeyDown(sf::Keyboard::RShift));
 		if (keyboard->isKeyPressed(sf::Keyboard::Space)) {
 			myMessage += char(32);
 		}
+		// Send and clear message if enter is pressed
 		if (keyboard->isKeyPressed(sf::Keyboard::Enter)) {
 			returnVal = new ChatMessageData();
 			returnVal->message = myMessage;
@@ -79,18 +91,22 @@ ChatMessageData* ChatWindow::update(float dt) {
 			myMessage = "Press 'T' to chat";
 			isChatting = false;
 		}
+		// Clear message and exist chat if escape is pressed
 		if (keyboard->isKeyPressed(sf::Keyboard::Escape)) {
 			myMessage = "Press 'T' to chat";
 			isChatting = false;
 			return returnVal;
 		}
+		// Clear last character if backspace is pressed
 		if (keyboard->isKeyPressed(sf::Keyboard::Backspace)) {
 			if (myMessage.size() > 0) {
 				myMessage.erase(myMessage.begin() + myMessage.size() - 1);
 			}
 		}
+		// Type out letters if letter key is pressed
 		for (int i = 0; i < 26; i++) {
 			int shift = 0;
+			// Subtract 32 from ASCII value if uppercase
 			if (keyboard->isKeyPressed(i)) {
 				if (isShiftDown) {
 					shift = 32;
@@ -98,6 +114,7 @@ ChatMessageData* ChatWindow::update(float dt) {
 				myMessage += char(97 + i - shift);
 			}
 		}
+		// Handle number key presses (either number, or appropriate symbol if shift is held)
 		for (int i = 0; i < 10; i++) {
 			if (keyboard->isKeyPressed(i + sf::Keyboard::Num0) || keyboard->isKeyPressed(i + sf::Keyboard::Numpad0)) {
 				if (!isShiftDown) {
@@ -108,6 +125,7 @@ ChatMessageData* ChatWindow::update(float dt) {
 				}
 			}
 		}
+		// Handle symbols not on a number key
 		for (int i = 0; i <= 11; i++) {
 			if (keyboard->isKeyPressed(sf::Keyboard::LBracket + i)) {
 				if (!isShiftDown) {
@@ -119,11 +137,14 @@ ChatMessageData* ChatWindow::update(float dt) {
 			}
 		}
 		// Putting a space at the end of a string does funky things for whatever reason
+		// Deletes the strange ghost character created when a space is added
 		if (myMessage.size() >= 2) {
 			if (myMessage.at(myMessage.size() - 1) < 32) {
 				myMessage.erase(myMessage.begin() + (myMessage.size() - 1));
 			}
 		}
+		// Sets a maximum chat message length
+		// Means that chat messages will never exceed the bounds of the chat window
 		myMessageText.setString(myMessage);
 		while (myMessageText.getLocalBounds().width > 460) {
 			myMessage.erase(myMessage.begin() + (myMessage.size()-1));
@@ -131,6 +152,7 @@ ChatMessageData* ChatWindow::update(float dt) {
 		}
 	}
 	else {
+		// If T is pressed, begin allowing text input into the chat
 		if (keyboard->isKeyPressed(sf::Keyboard::T)) {
 			isChatting = true;
 			myMessage = "";
@@ -139,6 +161,8 @@ ChatMessageData* ChatWindow::update(float dt) {
 	return returnVal;
 }
 
+// Renders the background window, and the last 16 messages, 
+// along with either a prompt to press T to chat, or the currently typed message
 void ChatWindow::render(sf::RenderWindow* window) {
 	window->draw(background);
 	for (int i = 0; i < messages.size(); i++) {
@@ -147,6 +171,7 @@ void ChatWindow::render(sf::RenderWindow* window) {
 	window->draw(myMessageText);
 }
 
+// Adds a message to the bottom of the chat list, and bumps up the others
 void ChatWindow::addMessage(ChatMessageData message) {
 	sf::Text* temp = messages[0];
 	temp->setString("[" + message.sender + "]: " + message.message);
@@ -156,21 +181,26 @@ void ChatWindow::addMessage(ChatMessageData message) {
 	updatePositions();
 }
 
+// Sets the keyboard pointer for handling chat input
 void ChatWindow::setKeyboard(Keyboard* ptr) {
 	keyboard = ptr;
 }
 
+// Sets the player data so it can be added to message data
+// Also used to set the colour of the messages sent by this player
 void ChatWindow::setPlayerData(PlayerData data) {
 	myMessageText.setFillColor(data.colour);
 	myName = data.name;
 }
 
+// Updates the Y positions of chat messages when a new one is added
 void ChatWindow::updatePositions() {
 	for (int i = 0; i < messages.size(); i++) {
 		messages[i]->setPosition(4, 4 + i * 16);
 	}
 }
 
+// Returns whether the chat is accepting text input or not
 bool ChatWindow::isChatFocused() {
 	return isChatting;
 }
--- Networking Project/Client.cpp
+++ Networking Project/Client.cpp
@@ -5,15 +5,19 @@ Client::Client() {
 }
 
 Client::~Client() {
+    // Clean up window
     if (window) {
         window->close();
         delete window;
     }
+    // Clean up socket
     socket.disconnect();
 }
 
+// Attempts to connect to server, and initialises SFML window
 bool Client::init() {
     std::string userInput = "";
+    // Asks the user for a valid server IP to connect to
     while (serverIP == sf::IpAddress::None) {
         std::cout << "Enter a valid ip address to connect to (or localhost to connect locally): ";
         std::cin >> userInput;
@@ -23,19 +27,26 @@ bool Client::init() {
         serverIP = sf::IpAddress(userInput);
     }
     userInput = "";
+    // Asks the user for a valid port to connect to
     while (!isValidPort(userInput)) {
         std::cout << "Enter a valid port number to connect to: ";
         std::cin >> userInput;
     }
     serverPort = atoi(userInput.data());
+    // Set socket to blocking for now until we successfully join server
+    // We don't have a window we have to update yet, so we want it to block 
     socket.setBlocking(true);
+    // Attempt to establish connection
     if (socket.connect(serverIP, serverPort) != sf::Socket::Done) {
+        // If it fails, likely due to invalid IP and/or port, return false which restarts this process
         std::cout << "Failed to connect to server" << std::endl;
         return false;
     }
     std::cout << "Successfully connected to server" << std::endl;
     sf::Color requestedColour;
     userInput = "";
+    // Ask the user to choose the colour of their penguin
+    // One colour component at a time
     while (!isValidColour(userInput)) {
         std::cout << "Enter a value of red for your penguin (0-255): ";
         std::cin >> userInput;
@@ -53,98 +64,133 @@ bool Client::init() {
         std::cin >> userInput;
     }
     requestedColour.b = atoi(userInput.data());
+    // Setup variables and packets for setting username
     bool usernameSuccess = false;
     sf::Packet usernamePacket;
     sf::Packet returnPacket;
+    // Repeat until success
     while (!usernameSuccess) {
+        // Reset variables
         usernamePacket.clear();
         returnPacket.clear();
         userInput = "";
+        // Ask user for a username until a valid one is entered
         while (!isValidUsername(userInput)) {
             std::cout << "Enter a username to connect to server (no spaces): ";
             std::cin >> userInput;
         }
+        // Pack the packet to send to server
         usernamePacket << userInput;
         usernamePacket << requestedColour;
+        // Attempt to send packet to server
         if (socket.send(usernamePacket) != sf::Socket::Done) {
+            // If the connection fails, restart connection process
             std::cout << "Failed to send username data to server" << std::endl;
             return false;
         }
         if (socket.receive(returnPacket) != sf::Socket::Done) {
+            // If the connection fails, restart connection process
+            // Socket is blocking, so only cause of status not being "Done" is disconnection or errors
             std::cout << "Failed to receive data from server" << std::endl;
             return false;
         }
+        // Unpack return packet
         PacketType returnType;
         returnPacket >> returnType;
         returnPacket >> usernameSuccess;
+        // If it isn't a usernameresponse packet, something has gone wrong, so try again
         if (returnType != PacketType::USERNAMERESPONSE) {
             usernameSuccess = false;
         }
+        // If the server rejects the username, then try again
         if (!usernameSuccess) {
             std::cout << "Username is invalid or already in use" << std::endl;
         }
     }
+    // Unpack player data from return packet
     PlayerData myData;
     returnPacket >> myData;
+    // Socket needs to no longer be blocking because we will have other stuff going on
     socket.setBlocking(false);
+    // Try to request a player list and positions of all players from server
     if (!requestPlayerData()) {
         std::cout << "Failed to request player data" << std::endl;
         return false;
     }
+    // Create SFML window
     window = new sf::RenderWindow(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Plub Cenguin");
     window->setFramerateLimit(60);
+    // Load textures
     penguinBaseTex.loadFromFile("graphics/BasePenguin.png");
     penguinColourTex.loadFromFile("graphics/PenguinColour.png");
+    // Initialise the player, and the chat window
     me.init(&penguinBaseTex, &penguinColourTex, myData);
     chat.setKeyboard(&keyboard);
     chat.setPlayerData(myData);
+    // Success, so return true and move to main loop
     return true;
 }
 
+// Main loop for client
 bool Client::update(float dt) {
+    // Handle window events, returns false if window was closed and closes program
     if (!windowEvents()) {
         return false;
     }
+    // Handle any received packets
     handleIncomingData();
+    // If the chat is not accepting text input, then handle player movement
     if (!chat.isChatFocused()) {
         me.update(dt, isFocused);
         movePacketCooldown -= dt;
+        // If the player moved, and hasn't sent a movement packet for a while, send packet
         if (me.movedThisFrame() && movePacketCooldown <= 0) {
             sendMovementPacket(true);
             movePacketCooldown = 0.05;
         }
+        // If the player stopped moving, send a stopped movement packet
+        // It is important that this gets sent because it tells interpolation calculations to stop predicting movement
         else if (me.stoppedThisFrame()) {
             sendMovementPacket(false);
             movePacketCooldown = 0;
         }
     }
+    // Handle movement updates for players other than the one controlled by the user
     for (int i = 0; i < players.size(); i++) {
         players[i]->update(dt);
     }
+    // Handle chat updates, and if a message should be sent, send it
     ChatMessageData* msg = chat.update(dt);
     if (msg) {
         sendMessage(*msg);
     }
+    // Update the keyboard state
     keyboard.update();
+    // Render everything
     render();
     return true;
 }
 
+// Handle SFML window events such as closing window, gaining or losing focus, and keyboard presses
 bool Client::windowEvents() {
     sf::Event event;
+    // Check for window events
     while (window->pollEvent(event))
     {
         switch (event.type) {
+            // If closed, close the window and exit the program
         case sf::Event::Closed:
             window->close();
             exit(0);
             break;
+            // Update whether the window has focus, used to determine whether to check input
         case sf::Event::GainedFocus:
             isFocused = true;
             break;
         case sf::Event::LostFocus:
             isFocused = false;
             break;
+            // Update keyboard states
         case sf::Event::KeyPressed:
             keyboard.setKeyDown(event.key.code);
             break;
@@ -153,23 +199,31 @@ bool Client::windowEvents() {
             break;
         }
     }
+    // If window has been closed, return false to exit program
     if (!window->isOpen()) {
         return false;
     }
     return true;
 }
 
+// Handle any incoming packets
 void Client::handleIncomingData() {
     sf::Packet packet;
     sf::Socket::Status status;
+    // Repeat until all packets are handled
     do {
+        // Reset packet
         packet.clear();
+        // Read incoming data
         status = socket.receive(packet);
+        // If disconnected from server, exit program
         if (status == sf::Socket::Error || status == sf::Socket::Disconnected) {
             std::cout << "Disconnected from server" << std::endl;
+            window->close();
             system("Pause");
             exit(0);
         }
+        // If has received any packets, handle it
         if (status == sf::Socket::Done) {
             PacketType type;
             packet >> type;
@@ -195,36 +249,49 @@ void Client::handleIncomingData() {
     } while (status == sf::Socket::Done);
 }
 
+// Render everything to the screen
 void Client::render() {
+    // Reset window
     window->clear(sf::Color::White);
     window->setView(sf::View(sf::Vector2f(960, 540), sf::Vector2f(1920, 1080)));
-    me.render(window);
+    // Render the players
     for (int i = 0; i < players.size(); i++) {
         players[i]->render(window);
     }
+    me.render(window);
+    // Render the chat window
+    // The background is transparent, so can be rendered on top
     chat.render(window);
+    // Push everything drawn to the screen
     window->display();
 }
 
+// Send a request to the server for player data
 bool Client::requestPlayerData() {
     std::cout << "Requesting player data" << std::endl;
     sf::Packet outPacket;
+    // Pack the packet
     outPacket << PacketType::REQUEST;
     outPacket << RequestType::PLAYERLIST;
     sf::Socket::Status status = socket.send(outPacket);
+    // If it fails to send, return false
     if (status == sf::Socket::Error || status == sf::Socket::Disconnected) {
         return false;
     }
     return true;
 }
 
+// When a list of player data is received from server, load it and initialise penguins for each
 void Client::loadPlayerList(sf::Packet packet) {
     std::cout << "Received player data from server" << std::endl;
     sf::Int8 playerCount;
+    // Get playercount from packet
     packet >> playerCount;
     for (int i = 0; i < playerCount; i++) {
+        // Unpack a player from the packet
         PlayerData player;
         packet >> player;
+        // If it's another player, create a penguin for them
         if (player.name != me.getName()) {
             NetworkPlayer* penguin = new NetworkPlayer();
             penguin->init(&penguinBaseTex, &penguinColourTex, player);
@@ -233,19 +300,24 @@ void Client::loadPlayerList(sf::Packet packet) {
     }
 }
 
+// When a new player connects, create a new penguin for them, and push message to chat
 void Client::loadNewConnectedPlayer(sf::Packet packet) {
     PlayerData player;
+    // Unpack packet
     packet >> player;
     std::cout << "Player [" << player.name << "] joined the server!" << std::endl;
+    // Check that somehow a duplicate player join packet hasn't been received
     if (player.name != me.getName()) {
         for (int i = 0; i < players.size(); i++) {
             if (players[i]->getName() == player.name) {
                 players.erase(players.begin() + i);
             }
         }
+        // Create a new penguin for the connecting player
         NetworkPlayer* penguin = new NetworkPlayer();
         penguin->init(&penguinBaseTex, &penguinColourTex, player);
         players.push_back(penguin);
+        // Push a new chat message letting the player know a new player has joined
         ChatMessageData msg;
         msg.sender = "SERVER";
         msg.colour = sf::Color::White;
@@ -254,61 +326,82 @@ void Client::loadNewConnectedPlayer(sf::Packet packet) {
     }
 }
 
+// When a disconnect packet is received from server, post chat message to server and get rid of their penguin
 void Client::unloadDisconnectedPlayer(sf::Packet packet) {
     std::string username;
+    // Unpack packet
     packet >> username;
     std::cout << "Player [" << username << "] left the server!" << std::endl;
     for (int i = 0; i < players.size(); i++) {
+        // Find matching player
         if (players[i]->getName() == username) {
+            // Post message to chat
             ChatMessageData msg;
             msg.sender = "SERVER";
             msg.colour = sf::Color::White;
             msg.message = "Player [" + username + "] left the server!";
             chat.addMessage(msg);
+            // Remove appropriate penguin from player vector
             players.erase(players.begin() + i);
         }
     }
 }
 
+// Sends a movement packet to the server, also tells whether the player is still moving or has stopped
 void Client::sendMovementPacket(bool moving) {
     sf::Packet packet;
     MovementData data;
+    // Pack packet with movement data
     sf::Vector2f playerPos = me.getPos();
     data.name = me.getName();
     data.x = playerPos.x;
     data.y = playerPos.y;
     data.moving = moving;
+    // Pack packet
     packet << PacketType::MOVEMENTDATA;
     packet << data;
     sf::Socket::Status status;
+    // Send packet
     status = socket.send(packet);
+    // If packet fails to send, post error message but this might not be catastrophic,
+    // If it is, will be handled at next cycle through main loop anyway
     if (status == sf::Socket::Disconnected || status == sf::Socket::Error) {
         std::cout << "Failed to send movement data to server, hopefully not problematic" << std::endl;
     }
 }
 
+// When movement data is received, pass it to appropriate penguin
 void Client::updatePlayerPosition(sf::Packet packet) {
     MovementData data;
+    // Unpack packet
     packet >> data;
     for (int i = 0; i < players.size(); i++) {
+        // Find matching player
         if (players[i]->getName() == data.name) {
+            // Pass data to penguin
             players[i]->setLatestData(data);
             //std::cout << "Received movement data for player " << data.name << ". Pos x = " << data.x << " y = " << data.y << ", moving = " << data.moving << std::endl;
         }
     }
 }
 
+// When chat message packet is received, pass it to the chat window for display
 void Client::printChatMessage(sf::Packet packet) {
     ChatMessageData data;
     packet >> data;
     chat.addMessage(data);
 }
 
+// Send message data to server
 void Client::sendMessage(ChatMessageData msg) {
     sf::Packet packet;
+    // Pack packet
     packet << PacketType::CHATMESSAGE;
     packet << msg;
+    // Send packet
     sf::Socket::Status status = socket.send(packet);
+    // If chat message fails to send, post error message
+    // If error is catastrophic, will be handled at next main loop cycle
     if (status == sf::Socket::Disconnected || status == sf::Socket::Error) {
         std::cout << "Failed to send chat message" << std::endl;
     }
