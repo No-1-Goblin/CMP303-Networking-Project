45c5c7891efa30ccc44559cbd9b7dbe4c3b803ea
Harry Ingham <98830464+No-1-Goblin@users.noreply.github.com>
2023-11-27 15:28:30 +0000

    Added more code comments

--- Networking Project/Keyboard.cpp
+++ Networking Project/Keyboard.cpp
@@ -1,6 +1,7 @@
 #include "Keyboard.h"
 
 Keyboard::Keyboard() {
+	// Set all key values to false
 	for (int i = 0; i < 256; i++) {
 		keys[i] = false;
 		keysLastFrame[i] = false;
@@ -11,24 +12,29 @@ Keyboard::~Keyboard() {
 
 }
 
+// Update what keys were pressed last frame
 void Keyboard::update() {
 	for (int i = 0; i < 256; i++) {
 		keysLastFrame[i] = keys[i];
 	}
 }
 
+// Returns whether the key was down this frame
 bool Keyboard::isKeyDown(int key) {
 	return keys[key];
 }
 
+// Returns whether the key was pressed this frame
 bool Keyboard::isKeyPressed(int key) {
 	return (keys[key] && !keysLastFrame[key]);
 }
 
+// Set the key to be held down
 void Keyboard::setKeyDown(int key) {
 	keys[key] = true;
 }
 
+// Set the key to no longer be held down
 void Keyboard::setKeyUp(int key) {
 	keys[key] = false;
 }
--- Networking Project/Main.cpp
+++ Networking Project/Main.cpp
@@ -6,15 +6,19 @@
 
 int main()
 {
+    // Initialise clock
     sf::Clock clock;
+    // Ask user whether to host server or join server
     std::string userInput = "";
     while (userInput != "host" && userInput != "join") {
         std::cout << "Would you like to 'host' or 'join'? ";
         std::cin >> userInput;
     }
     bool successfulBoot = false;
+    // If hosting a server
     if (userInput == "host") {
         Server* server = nullptr;
+        // Attempt to boot server
         while (!successfulBoot) {
             server = new Server();
             if (server->init()) {
@@ -24,12 +28,16 @@ int main()
                 delete server;
             }
         }
+        // Server main loop
+        // Exit code is within server itself
         while (true) {
             server->tick(clock.restart().asMilliseconds());
         }
     }
+    // If joining a server
     else {
         Client* client = nullptr;
+        // Attempt to join a server
         while (!successfulBoot) {
             client = new Client();
             if (client->init()) {
@@ -39,6 +47,7 @@ int main()
                 delete client;
             }
         }
+        // Client main loop
         while (client->update(clock.restart().asSeconds()));
     }
     return 0;
