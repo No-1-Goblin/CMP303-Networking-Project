dcc25cbd71d2dc82a2970ac589d8d5bd35d8d4eb
Harry Ingham <98830464+No-1-Goblin@users.noreply.github.com>
2023-12-04 15:02:14 +0000

    Finished commenting

--- Networking Project/Server.cpp
+++ Networking Project/Server.cpp
@@ -1,17 +1,24 @@
 #include "Server.h"
 
 Server::Server() {
+	// Initialise RNG
 	srand(time(0));
 }
 
+// Clean up sockets
 Server::~Server() {
 	listenerSocket.close();
 	for (int i = 0; i < connectionQueue.size(); i++) {
 		connectionQueue[i]->disconnect();
 	}
+	for (int i = 0; i < players.size(); i++) {
+		players[i].first->disconnect();
+	}
 }
 
+// Initialise listener socket and bind to port
 bool Server::init() {
+	// Get a port to host server on
 	std::string userInput = "";
 	while (!isValidPort(userInput)) {
 		std::cout << "Enter a valid port number to host the server on (or 'any' to use any): ";
@@ -21,18 +28,23 @@ bool Server::init() {
 		}
 	}
 	port = atoi(userInput.data());
+	// Set listener socket to not block
 	listenerSocket.setBlocking(false);
+	// Begin listening on port
 	if (listenerSocket.listen(port) == sf::Socket::Error) {
 		std::cout << "Failed to open port" << std::endl;
 		return false;
 	}
 	port = listenerSocket.getLocalPort();
 	std::cout << "Successfully opened listener socket on port " << port << std::endl;
+	// Add listener socket to the selector
 	selector.add(listenerSocket);
 	return true;
 }
 
+// Main server loop
 void Server::tick(float dt) {
+	// Use selector to wait for a socket to be ready for data to be read
 	if (selector.wait()) {
 		if (selector.isReady(listenerSocket)) {
 			handleNewConnections();
@@ -44,11 +56,16 @@ void Server::tick(float dt) {
 	}
 }
 
+// Handle any new connections to the listener socket
 void Server::handleNewConnections() {
 	sf::TcpSocket* client = new sf::TcpSocket;
+	// Attempt to accept connection from new client
 	if (listenerSocket.accept(*client) == sf::Socket::Done) {
+		// Set the socket to be non-blocking
 		client->setBlocking(false);
+		// Add the socket to the queue for selecting a username and colour
 		connectionQueue.push_back(client);
+		// Add the socket to the selector
 		selector.add(*client);
 		std::cout << "New attempt to connect from address: " << client->getRemoteAddress() << ":" << client->getRemotePort() << std::endl;
 	}
@@ -57,12 +74,16 @@ void Server::handleNewConnections() {
 	}
 }
 
+// Handle any users who have established a connection, but still need to choose a username and colour
 void Server::handleConnectionQueue() {
 	for (int i = 0; i < connectionQueue.size(); i++) {
+		// Find the ready socket
 		if (selector.isReady(*connectionQueue[i])) {
 			sf::Packet packet;
+			// Check status of the connection
 			sf::Socket::Status status = connectionQueue[i]->receive(packet);
 			switch (status) {
+				// If connection has failed for whatever reason, disconnect client and delete socket
 			case sf::Socket::Disconnected:
 				delete connectionQueue[i];
 				connectionQueue.erase(connectionQueue.begin() + i);
@@ -73,6 +94,7 @@ void Server::handleConnectionQueue() {
 				connectionQueue.erase(connectionQueue.begin() + i);
 				i--;
 				break;
+				// If successfully received data, read it
 			case sf::Socket::Done:
 				std::string username;
 				packet >> username;
@@ -80,6 +102,7 @@ void Server::handleConnectionQueue() {
 				packet >> colour;
 				sf::Packet returnPacket;
 				sf::TcpSocket* socket = connectionQueue[i];
+				// Check validity of received username data
 				if (isValidUsername(username)) {
 					bool exists = false;
 					for (int i = 0; i < usernames.size(); i++) {
@@ -87,7 +110,9 @@ void Server::handleConnectionQueue() {
 							exists = true;
 						}
 					}
+					// If username is valid and not in use, save player data and pack return packet
 					if (!exists) {
+						// Push the player to the player list
 						players.push_back(std::pair<sf::TcpSocket*, std::string>(socket, username));
 						usernames.push_back(username);
 						PlayerData data;
@@ -96,37 +121,46 @@ void Server::handleConnectionQueue() {
 						data.y = rand() % 1080;
 						data.colour = colour;
 						playerData.push_back(data);
+						// Pack return packet
 						returnPacket << PacketType::USERNAMERESPONSE;
 						returnPacket << true;
 						returnPacket << data;
+						// Remove from connection queue, since they are now a connected player
 						connectionQueue.erase(connectionQueue.begin() + i);
 						i--;
 						std::cout << "Player [" << username << "] joined the server!" << std::endl;
+						// Send connection notification to other players
 						sf::Packet connectNotifyPacket;
 						connectNotifyPacket << PacketType::CONNECTNOTIFICATION;
 						connectNotifyPacket << data;
 						broadcastPacket(connectNotifyPacket, username);
 					}
 				}
+				// Send return packet to user
 				socket->send(returnPacket);
 			}
 		}
 	}
 }
 
+// Handle incoming data from connected players
 void Server::handleIncomingData() {
 	sf::Socket::Status status;
 	for (int i = 0; i < players.size(); i++) {
+		// Find socket that has received data
 		if (selector.isReady(*players[i].first)) {
 			do {
 				sf::TcpSocket* socket = players[i].first;
 				std::string username = players[i].second;
 				sf::Packet packet;
+				// Check whether data was successfully received
 				status = socket->receive(packet);
 				if (status == sf::Socket::Disconnected || status == sf::Socket::Error) {
+					// Disconnect if connection has failed
 					closeConnection(username);
 					break;
 				}
+				// If successful, pass the data to the appropriate handler function
 				if (status == sf::Socket::Done) {
 					PacketType type;
 					packet >> type;
@@ -147,28 +181,38 @@ void Server::handleIncomingData() {
 	}
 }
 
+// Sends the packet to every single player
 void Server::broadcastPacket(sf::Packet packet) {
 	for (int i = 0; i < players.size(); i++) {
+		// Send packet to player
+		// We aren't error checking here as any disconnects will be caught in the next main loop iteration
 		players[i].first->send(packet);
 	}
 }
 
+// Sends the packet to every player, except the specified player
 void Server::broadcastPacket(sf::Packet packet, std::string exception) {
 	for (int i = 0; i < players.size(); i++) {
+		// Check player isn't the specified exception
 		if (players[i].second != exception) {
+			// Send packet to player
+			// We aren't error checking here as any disconnects will be caught in the next main loop iteration
 			players[i].first->send(packet);
 		}
 	}
 }
 
+// Disconnect the specified player and remove them from any vectors
 void Server::closeConnection(std::string username) {
 	std::cout << "Player [" << username << "] disconnected." << std::endl;
+	// Find player in username list and erase them
 	for (int i = 0; i < usernames.size(); i++) {
 		if (usernames[i] == username) {
 			usernames.erase(usernames.begin() + i);
 			break;
 		}
 	}
+	// Find player in player list and close socket, then erase them
 	for (int i = 0; i < players.size(); i++) {
 		if (players[i].second == username) {
 			selector.remove(*players[i].first);
@@ -177,23 +221,27 @@ void Server::closeConnection(std::string username) {
 			break;
 		}
 	}
+	// Find player in playerdata list, and erase them
 	for (int i = 0; i < playerData.size(); i++) {
 		if (playerData[i].name == username) {
 			playerData.erase(playerData.begin() + i);
 			break;
 		}
 	}
+	// Send disconnect notification to all players
 	sf::Packet packet;
 	packet << PacketType::DISCONNECTNOTIFICATION;
 	packet << username;
 	broadcastPacket(packet);
 }
 
+// Handle packets that are of the type "request"
 void Server::handleRequest(sf::Packet requestPacket, sf::TcpSocket* socket) {
 	RequestType type;
 	requestPacket >> type;
 	sf::Packet outPacket;
 	bool success = false;
+	// Check what type of request it is, and prepare the appropriate response packet
 	switch (type) {
 	case RequestType::PLAYERLIST:
 		outPacket << PacketType::PLAYERLIST;
@@ -203,15 +251,18 @@ void Server::handleRequest(sf::Packet requestPacket, sf::TcpSocket* socket) {
 		}
 		success = true;
 	}
+	// Send return packet
 	if (success) {
 		socket->send(outPacket);
 	}
 }
 
+// Handle movement data received from a player, and forward it to every other player
 void Server::handleMovementData(sf::Packet packet) {
 	MovementData data;
 	packet >> data;
 	bool validPlayer = false;
+	// Save to the server's copy of player data
 	for (int i = 0; i < playerData.size(); i++) {
 		if (data.name == playerData[i].name) {
 			playerData[i].x = data.x;
@@ -220,6 +271,7 @@ void Server::handleMovementData(sf::Packet packet) {
 			break;
  		}
 	}
+	// Forward to other players
 	if (validPlayer) {
 		sf::Packet outPacket;
 		outPacket << PacketType::MOVEMENTDATA;
